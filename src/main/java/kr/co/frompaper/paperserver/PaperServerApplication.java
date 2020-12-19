3ea62b903e15b3c8296dd43f27d80ff3cbd0cb63
jinsauk <tifkshtmxm@gmail.com>
2020-12-19 14:59:39 +0900

    feat: spring initializer

---
+++ src/main/java/kr/co/frompaper/paperserver/PaperServerApplication.java
@@ -0,0 +1,13 @@
+package kr.co.frompaper.paperserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/frompaper/paperserver/PaperServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.frompaper.paperserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
